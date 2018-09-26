d98ddac12e4b6cc847b2f61872d20f9493435070
Nick <nicholaswmazzei@gmail.com>
2018-09-26 01:32:21 -0300

    Hide no formCCO com JS

---
+++ App/static/js/FormCCO.js
@@ -0,0 +1,9 @@
+class FormCCO{
+
+    constructor(){
+        this._analises = document.querySelectorAll('.analise');
+        this._btn_hide = document.querySelectorAll('.botao-hide');
+        console.log(this._analises);
+        console.log(this._btn_hide);
+    }
+}
